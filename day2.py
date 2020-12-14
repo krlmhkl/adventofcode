b86248bddf6b2e7a7c4eb22b4ed7e10a4378392a
krlmhkl <71080369+krlmhkl@users.noreply.github.com>
2020-12-14 14:37:01 +0200

    day7 done. try day4 in js

--- day2.py
+++ day2.py
@@ -6,7 +6,7 @@ n = 0
 
 # part 1
 
-""" for el in lines:
+for el in lines:
     line = el.split()
     min, max = [int(el) for el in line[0].split("-")]
     char = line[1].strip(":")
@@ -14,14 +14,14 @@ n = 0
     if pw.count(char) <= max and pw.count(char) >= min:
         x += 1
         n += 1
-        print(x) 
+        print(x)
     else:
         print(x)
         n += 1
-     """
+
 
 # part 2
-
+"""
 for el in lines:
     line = el.split()
     min, max = [int(el) for el in line[0].split("-")]
@@ -30,7 +30,7 @@ for el in lines:
     if char is pw[max - 1] and char is not pw[min - 1]:
         x += 1
         n += 1
-        print(x) 
+        print(x)
     elif char is pw[min - 1] and char is not pw[max - 1]:
         x += 1
         n += 1
@@ -38,3 +38,4 @@ for el in lines:
     else:
         print(x)
         n += 1
+"""
