2e0bcbaf4e106e94d6b2dadd1f432def654621ef
Karl-Mihkel Truu <71080369+krlmhkl@users.noreply.github.com>
2020-12-21 23:24:09 +0200

    Update papersplease-day4.py

--- papersplease-day4.py
+++ papersplease-day4.py
@@ -1,11 +1,27 @@
 inputfile = open("input-day4.txt", "r")
-airportSecurity = inputfile.read()
-
-passport = dict([el.split(":") for el in airportSecurity.split()])
-print(passport)
+#airportSecurity = inputfile.read().split("\n\n")
+batches = [passports.split() for passports in inputfile.read().split("\n\n")]
 
 
+for batch in batches:
+    passport = dict([el.split(":") for el in batch])
+#    print(passport)
 
+
+x = list(passport.keys())
+x.sort()
+print(str(x)) #tee listiks
+
+
+# if ["iyr" and "byr" and "eyr" and "hgt" and "hcl" and "ecl" and "pid"] in x: counter +=1
+# if ["iyr" and "byr" and "eyr" and "hgt" and "hcl" and "ecl" and "pid" and "cid"] in x: counter +=1 
+
+# sorteeri, tähestiku järjekorras
+
+
+
+#passport = dict([el.split(":") for el in batches.split()])
+#print(passport)
 
 
 #valid_ecl = ["gry", "blu", "red"]
